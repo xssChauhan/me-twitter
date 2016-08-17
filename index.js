4263493f2477631bd39bd5337c17c314bc524455
Shikhar <shikhar.chauhan@live.com>
2016-08-18 01:37:35 +0530

    Finish touches to login

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ var express = require("express"),
 	controllers = include("controllers");
 
 var app = express();
+var path = require("path");
 
 controllers(app);
 
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname+"/index.html"));
+});
 app.listen(process.env.PORT || 3000,function(){
 	console.log("Listening on " + process.env.port);
-})
+});
